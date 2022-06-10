490b6021614279b68560bd126fcae049bc7ee4b6
lundbmp <m.lundberg008@gmail.com>
2022-06-09 21:50:50 -0500

    made connections with API, producing good data with async/await.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
-/*
-http://api-doc.axesso.de/
-https://openlibrary.org/developers/api
-*/
+var historyArray;
+var displayArray = [];
 
-let apiurl = 'https://www.googleapis.com/books/v1/volumes?q=lord+of+the+rings';
+// object to house book and drink combo info
+function cardObj(bookObj, drinkObj) {
+    // TODO: assign data from bookJSONObj and drinkJSONObj
 
-fetch(apiurl)
-    .then(function(reponse) {
-        return reponse.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        return fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-    })
-    .then(function(reponse) {
-        return reponse.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        console.log("help");
-    })
+}
+
+
+async function callApiPromise(bookTitle) {
+    let googleBooksApi = 'https://www.googleapis.com/books/v1/volumes?q=' + bookTitle;
+    let cocktailDbApi = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    let cocktailDbResponse;
+    let cocktailDbJson;
+
+    // fetching array of books based on bookTitle search
+    let googleResponse = await fetch(googleBooksApi);
+    let googleJson = await googleResponse.json();
+
+    // loop through to produce 5 books from google api and fetch random cocktail
+    for(let i = 0; i < 5; i++) {
+        console.log(googleJson);
+        cocktailDbResponse = await fetch(cocktailDbApi);
+        cocktailDbJson = await cocktailDbResponse.json();
+        console.log(cocktailDbJson);
+    }
+}
